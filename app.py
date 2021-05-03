9286318868a293f7a26774a4c8a0ae49690edb67
Zack <zacklindberg87@gmail.com>
2021-05-03 19:06:45 -0400

    fixed 500 error when app starts

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from db import db
+
 from flask import Flask
 from flask_restful import Api # type: ignore
 from flask_jwt import JWT # type: ignore
@@ -20,10 +20,9 @@ api.add_resource(ItemList, "/items")
 api.add_resource(StoreList, "/stores")
 api.add_resource(UserRegister, "/register")
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
+
 
 if __name__ == "__main__":
+    from db import db
     db.init_app(app)
     app.run(port=5000, debug=True)
